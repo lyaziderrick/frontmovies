35121dad8a877a4e64ac31f2101a7d0302bc39d1
unknown <elvismugerwa88@gmail.com>
2022-07-08 06:51:34 -0700

    add all

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -22,7 +22,7 @@ function Search() {
   React.useEffect(() => {
    home()
   }, []);
- 
+  if (!post) return null;
 
   return (
     <div className="centerem">
@@ -30,34 +30,7 @@ function Search() {
     <h1>LdexMovies</h1> 
     <h5>Find Movies, TV shows and more</h5>  
     </div>  
-     if (!post) return null;
-    <div className="imageone">
-    <input value={value} onChange={(e) => {setValue(e.target.value)}} />
-    <button type="submit" className="btn" onClick={home}>Search</button>  
     </div>
-    <div><h1>{value}</h1></div>
-    <div class="result">  
-      <div class="see">  
-        
-     <img src ={post.Poster} alt="N/A" onClick={() => setShowText(!showText)} />
-     
-        </div>
-        <div>
-        <h5>{post.Plot}</h5>
-       </div>
-       <div>
-        <h6>Realise:{post.Year}</h6>
-        <h6>Genre:{post.Genre} </h6>
-        <h6>Country:{post.Country}</h6>
-        <h6>Runtime:{post.Runtime} </h6>
-       </div>
-      
-    </div>
-    <div class="displayten">
-    {showText &&  <iframe id="ve-iframe" src={url} 
-      scrolling="no"  allowfullscreen="allowfullscreen" frameborder="0"></iframe>}
-       </div>
-   </div>
    
   );
 }
