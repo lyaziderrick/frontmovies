21ad0448e78ce7633f386197dd8b5480f9e2fef6
unknown <elvismugerwa88@gmail.com>
2022-07-08 07:06:34 -0700

    add all

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -12,7 +12,7 @@ function Search() {
   const [post, setPost] = React.useState(null);
   const [showText, setShowText] = useState(false);
   
-  const baseURL = "http://www.omdbapi.com/?t= "+value.replace(/ /g,"+")+"+&apikey=583d761d";
+  const baseURL = "https://fmovies.co/film/best-years-gone-1630853679?play=1/";
   const url="https://fmovies.co/film/best-years-gone-1630853679?play=1/";
   const home = () => {  
     axios.get(baseURL).then((response) => {
@@ -30,7 +30,33 @@ function Search() {
     <h1>LdexMovies</h1> 
     <h5>Find Movies, TV shows and more</h5>  
     </div>  
+    <div className="imageone">
+    <input value={value} onChange={(e) => {setValue(e.target.value)}} />
+    <button type="submit" className="btn" onClick={home}>Search</button>  
     </div>
+    <div><h1>{value}</h1></div>
+    <div class="result">  
+      <div class="see">  
+        
+     <img src ={post.Poster} alt="N/A" onClick={() => setShowText(!showText)} />
+     
+        </div>
+        <div>
+        <h5>{post.Plot}</h5>
+       </div>
+       <div>
+        <h6>Realise:{post.Year}</h6>
+        <h6>Genre:{post.Genre} </h6>
+        <h6>Country:{post.Country}</h6>
+        <h6>Runtime:{post.Runtime} </h6>
+       </div>
+      
+    </div>
+    <div class="displayten">
+    {showText &&  <iframe id="ve-iframe" src={url} 
+      scrolling="no"  allowfullscreen="allowfullscreen" frameborder="0"></iframe>}
+       </div>
+   </div>
    
   );
 }
