d688c19d3f08889601dab8645409d96293dd87fc
unknown <elvismugerwa88@gmail.com>
2022-07-31 00:56:16 -0700

    depoly all

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -12,31 +12,7 @@ import Video from './video';
 
 const Search = (props) => {
   
-  const[value, setValue] = useState(""); 
-  var CurrentYear = new Date().getFullYear()
-  const[yvalue, ysetValue] = useState(""); 
-  const [post, setPost] = React.useState(null);
-  const [showText, setShowText] = useState(false);
-  const baseURL = "";
-  const baseURLx = "";  
   
-  const url="https://2embed.org/embed/";
-  const home = () => { 
-    if([props.messagex]  !== ""){ 
-    axios.get(baseURLx).then((response) => {
-        setPost(response.data);
-      });
-    }
-      if(value  !== ""){ 
-        axios.get(baseURL).then((response) => {
-            setPost(response.data);
-          }); 
-        }
-  }
-  React.useEffect(() => {
-   home()
-  }, []);
-  if (!post) return post;
 
   return (
     <div className="centerem">
@@ -45,31 +21,28 @@ const Search = (props) => {
     <h5>Find Movies, TV shows and more</h5>  
     </div>  
     <div className="imageone">
-    <input value={value} onChange={(e) => {setValue(e.target.value)}} placeholder="Movie"/>
-    <input value={yvalue} onChange={(e) => {ysetValue(e.target.value)}} placeholder="year"/>
-    <button type="submit" className="btn" onClick={home}>Search</button>  
+    <input value="name"  placeholder="Movie"/>
+    <input value="year" placeholder="year"/>
+    <button type="submit" className="btn" >Search</button>  
     </div>
     <div><h1>{props.messagex}</h1></div>
     <div><h5>Click Search Button</h5></div>
 
     <div class="result">  
       <div class="see">   
-     <img src ={post.Poster} alt="N/I" onClick={() => setShowText(!showText)} />
+     <img src ="" alt="N/I"  />
      
         </div>
         <div>
-        <h5>{post.Plot}</h5>
+     
        </div>
        <div>
-        <h6>Realise:{post.Year}</h6>
-        <h6>Genre:{post.Genre} </h6>
-        <h6>Country:{post.Country}</h6>
-        <h6>Runtime:{post.Runtime} </h6>
+        
         
        </div>  
     </div>
     <div class="displayten"> 
-    <Video message={url + post.imdbID}/>   
+     
     </div>
 
    </div>
