6cf9811fc89f21c34ed9a2f8c6ff6c4f833ec83e
unknown <elvismugerwa88@gmail.com>
2022-08-11 09:34:14 -0700

    add all

--- src/components/adsense/adsense.js
+++ src/components/adsense/adsense.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import {Adsense} from '@ctrl/react-adsense';
+import React, { Component  } from 'react'
 
-<Adsense
-  client="ca-pub-5325051597778065"
-  slot="7730047444"
-  style={{ width: 500, height: 300 }}
-  format=""
-/>
+class Adsense extends Component {
+
+    componentDidMount() {
+     (window.adsbygoogle = window.adsbygoogle || []).push({})
+    }
+
+   render () {
+    return(
+        <div>
+        <ins className = "adsbygoogle"
+                style = { {display:"inline-block",width:"728px",height:"90px"} }
+                data-ad-client = "ca-pub-5325051597778065"
+                data-ad-slot = "7730047444"></ins>
+        </div>)
+    }
+}
 
 export default Adsense
