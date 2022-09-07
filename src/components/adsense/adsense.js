c8e9b6467c41baac0d573cadb86209341d94aa62
unknown <elvismugerwa88@gmail.com>
2022-09-07 02:48:57 -0700

    add all

--- src/components/adsense/adsense.js
+++ src/components/adsense/adsense.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef } from 'react'
-import "./adsense.css"
-export default function Adzterra(){
+export default function Adzterra(): JSX.Element{
 const banner = useRef<HTMLDivElement>(null)
 const atOptions = {
         key: '9091cd7be08106d4a0509ea9a0b163cb',
@@ -10,21 +9,23 @@ const atOptions = {
         params: {},
 }
     useEffect(() => {
-        if (banner.current.Child) {
+   
         const conf = document.createElement('script')
-        const script = document.createElement('script')    
-        script.type = 'text/javascript'
-        script.src = `//pl17662934.profitablegatetocontent.com/${atOptions.key}/invoke.js`
+        const script = document.createElement('script')
+        script.type = 'text/javascript'  
+        script.src = `//pl17680140.profitablegatetocontent.com/${atOptions.key}/invoke.js
+        <div id="container-9091cd7be08106d4a0509ea9a0b163cb"></div>
+        `
         conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`
+
         if (banner.current) {
             banner.current.append(conf)
             banner.current.append(script)
-        } 
+        
     }
 }, [])
-return <div>
+return( <div>
     {banner}
-    <div id="container-9091cd7be08106d4a0509ea9a0b163cb"></div>
-    </div>
+    </div>)
     
 }
