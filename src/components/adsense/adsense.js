e6f397d87f03ba54a97c2179f64d2a264a538ace
unknown <elvismugerwa88@gmail.com>
2022-09-06 22:28:06 -0700

    add all

--- src/components/adsense/adsense.js
+++ src/components/adsense/adsense.js
@@ -14,7 +14,9 @@ const atOptions = {
         const conf = document.createElement('script')
         const script = document.createElement('script')    
         script.type = 'text/javascript'
-        script.src = `//pl17662934.profitablegatetocontent.com/${atOptions.key}/invoke.js`
+        script.src = `//pl17662934.profitablegatetocontent.com/${atOptions.key}/invoke.js 
+        <div id="container-0e9ed3eef5346b35f6749a32bb5a8340"></div>
+        `
         conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`
         if (banner.current) {
             banner.current.append(conf)
