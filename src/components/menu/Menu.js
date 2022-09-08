f8e6f7468b64fb361b25e02a51c6153ea139deae
unknown <elvismugerwa88@gmail.com>
2022-09-08 00:28:37 -0700

    add all

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,7 +1,6 @@
 import React from "react"
 import {Nav,NavLink,Bars ,NavMenu,NavBtn,NavBtnLink} from "./NavbarElements"
 import ME from "./logo.png"
-import Adsense from "../adsense/adsense"
 function Menu(){
     return(
          
@@ -9,7 +8,7 @@ function Menu(){
   
    <Nav>
     <NavLink to="/">
-  <img src={ME} alt="logo"/>
+  
     </NavLink>
     <Bars/>
     <NavMenu>
